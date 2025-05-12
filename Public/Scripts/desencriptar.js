9ef33c4d5f342d6d9883ed82e21c7e481a3c805d
NicolasYanB <nicolas07.yan@gmail.com>
2025-05-12 01:44:09 -0300

    desencripta arquivos grandes

--- Public/Scripts/desencriptar.js
+++ Public/Scripts/desencriptar.js
@@ -1,13 +1,43 @@
 const desencriptarBtn = document.getElementById("desencriptar-btn");
+let text;
+const fileInput = document.getElementById("arquivo-desencriptar");
+if (fileInput.files.length > 0) {
+  fileInput.files.item(0).text().then(t => {
+    text = t.slice(0, -1);
+  })
+}
+fileInput.onchange = (event) => {
+  event.target.files.item(0).text().then((txt) => {
+    text = txt.slice(0, -1);
+  });
+}
 
-desencriptarBtn.onclick = (event) => {
+desencriptarBtn.onclick = (_) => {
   const decode = Module.cwrap('decode', 'number', ['string', 'string', 'string', 'string']);
   const p = document.getElementById("decrypt-p").value;
   const q = document.getElementById("decrypt-q").value;
   const e = document.getElementById("decrypt-e").value;
-  const text = document.getElementById("encryptedText").value;
-  decode(text, p, q, e);
-  const f = Module.FS.readFile('decode.txt', {encoding: 'utf8'});
+  if (!text) {
+    text = document.getElementById("encryptedText").value;
+  }
+  const splittedText = text.split('-');
+  const size = splittedText.length;
   const output = document.getElementById("decryptedOutput");
-  output.innerText = f;
+  if (size >= 10000) {
+    const bound = Math.ceil(Math.sqrt(size));
+    let decodedText = '';
+    for (let i = 0; i <= bound+1; i++) {
+      const chunk = splittedText.slice(i*bound, (i+1)*bound).join('-');
+      decode(chunk, p, q, e);
+      const f = Module.FS.readFile('decode.txt', {encoding: 'utf8'});
+      decodedText += f;
+      console.log(`${i}/${bound+1}`);
+    }
+    encryptedOutput.innerHTML = "O texto é muito grande para ser apresentado."
+    download(decodedText, 'decoded.txt');
+  } else {
+    decode(text, p, q, e);
+    const f = Module.FS.readFile('decode.txt', {encoding: 'utf8'});
+    output.innerText = f;
+  }
 }
